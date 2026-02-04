ef3b668b39caa6597a2179dc4db0209cd3b1ffa1
Luciano Tito 1801 <lucianotitocedron@gmail.com>
2026-02-04 20:54:11 -0300

    Tipos de clases

---
+++ POO ++/Tipos de clases/Tipos de clases/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tipos_de_clases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Developer developer1 = new Developer();
+            Lider lider1 = new Lider();
+            
+
+        }
+    }
+}
