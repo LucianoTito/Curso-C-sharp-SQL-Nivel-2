b3114c65d5cc835c0a40b44ab32822a3ebe5b487
Luciano Tito 1801 <lucianotitocedron@gmail.com>
2026-02-05 22:58:51 -0300

    Utilidades de datetime y explicación

---
+++ WinForms/Tipo Date Time/Tipo Date Time/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tipo_Date_Time
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaración de una variable de tipo DateTime e invoco a un constructor para asignarle un valor
+            DateTime fecha = new DateTime(1993, 01, 18);
+
+            // Imprimo el valor de la variable
+            Console.WriteLine("Valor de la variable fecha: " + fecha.ToString());
+
+            // Obtengo la fecha y hora actual del sistema:
+            DateTime fechaActual = DateTime.Now;
+            Console.WriteLine("Fecha y hora actual: " + fechaActual.ToString());
+
+
+            //Como agregarle días a una fecha
+            DateTime fechaMasCincoDias = fecha.AddDays(5);
+            Console.WriteLine("Fecha más cinco días: " + fechaMasCincoDias.ToString());
+
+            //Como mostrar solo la fecha sin la hora
+            Console.WriteLine("Solo la fecha: " + fechaActual.ToShortDateString());
+
+        }
+    }
+}
